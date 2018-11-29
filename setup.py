d5d5e87195c2bd8b9cdc50fec54b8a55697d122c
Yuya Goto <y_goto@pkshatech.com>
2018-11-29 17:12:30 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='jdeppy',
+    version='0.1',
+    description='Python wrapper for J.DepP',
+    url='https://github.com/matsurihime/jdeppy',
+    author='matsurihime',
+    author_email='pipikapur@gmail.com',
+    license='MIT',
+    keywords='jdepp',
+    packages=find_packages(),
+    classifiers=[
+        'Natural Language :: Japanese',
+    ],
+)
